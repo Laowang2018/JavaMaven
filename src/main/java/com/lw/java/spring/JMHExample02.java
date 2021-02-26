6025837fd8273e3e191d1da127cf49bbe6b13ab5
Laowang <wsj6051@126.com>
2021-02-27 02:55:11 +0800

    add JMHExample02.java

---
+++ src/main/java/com/lw/java/spring/JMHExample02.java
@@ -0,0 +1,30 @@
+package com.lw.java.spring;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@State(Scope.Thread)
+public class JMHExample02 {
+    @Benchmark
+    public void normalMethod() {
+
+    }
+
+    public static void main(String[] args) throws RunnerException {
+       final Options opts = new OptionsBuilder()
+                .include(JMHExample02.class.getSimpleName())
+                .forks(1)
+                .measurementIterations(10)
+                .warmupIterations(5)
+                .build();
+       new Runner(opts).run();
+    }
+
+}
